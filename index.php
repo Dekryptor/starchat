f04d607573fdf529a1f6840975a007e4fd7ed44a
SadError256 <hyland.belcher@yahoo.com>
2018-09-15 19:22:18 -0400

    Removed encrypting username lol

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$doesexist = $conn->query("SELECT id, firstname, password, anonid FROM accounts WHERE firstname = '".$conn->real_escape_string(crypt($_POST["username"]))."'");
+$doesexist = $conn->query("SELECT id, firstname, password, anonid FROM accounts WHERE firstname = '".$conn->real_escape_string($_POST["username"])."'");
 
 $row = $doesexist->fetch_array(MYSQLI_NUM);
 
