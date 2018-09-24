42e82e6f2e881001c5d1bd32fd75d2a4ea69bdc2
SadError256 <hylandb256@gmail.com>
2018-09-24 19:49:46 -0400

    Readmessage and addcontact support with message support

--- api.php
+++ api.php
@@ -25,7 +25,7 @@ exit();
 
 // we need the function below to store conversation id
 function generateRandomString($length = 25) {
-    $characters = '0123456789!?-+=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $characters = '0123456789-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
     for ($i = 0; $i < $length; $i++) {
@@ -34,8 +34,32 @@ function generateRandomString($length = 25) {
     return $randomString;
 }
 
+if (isset($_GET["getcontacts"])) {
+  // Before you ask, we wont pass a html stripper from this, since we dont want clients dealing with pain of striping &amp like things and stuff, and for web clients no harm will be done
+  // Should i remove first 5 characters from contacts, since users first ever contact is probably just 5 ampersigns, but if we add contact removal this can become a tragedy
+  die($qcontact);
+}
+
+if (isset($_GET["readmessages"])) {
+  // to prevent reading of unauthorized files, we will check if valid id, like for example, some could input ../../../sensitiveinfo.php and get access to that, lets prevent this from happening
+  if(preg_match('/[a-zA-Z0-9\-]{3,40}$/', $_GET["readmessages"])) {
+    // yep, seems safe enough
+  }else{
+    die("Not a valid id, nice try though.");
+  }
+  if(preg_match('/\.php$/', $_GET["readmessages"])) {
+    // if user bypasses, at least check if .php is disabled, most sensitive info for the most part is in php files
+  }else{
+    die("final check failed, what");
+  }
+  // todo, extra important things to check if really is a valid id, for now above will work, but what if above fails, an error would probably get reported.
+  // i know i sound paranoid or something, but someday, someone will figure out how to bypass this, thats when maximum security is needed
+  // i mean cmon, some things have mysql passwords in plain text, a mysql password found is a whole life wasted
+  die(htmlspecialchars(file_get_contents("convos/.ht".$_GET["readmessages"])));
+}
+
 // preferably, we will try to keep conversations in one box, so if one person deletes the convo, it deletes it for other user as well
-$filename = $qanonid . "......." . $qusername . "......." . htmlspecialchars($_GET["contacter"]) + ".txt";
+// $filename = $qanonid . "......." . $qusername . "......." . htmlspecialchars($_GET["contacter"]) + ".txt";
 
 if (isset($_GET["addcontact"])) {
 
@@ -46,9 +70,9 @@ $quickcheck = $conn->query("SELECT firstname, id FROM accounts WHERE firstname =
 $somedata = $quickcheck->fetch_array(MYSQLI_NUM);
 
 if (count($somedata)>0) {
-  // move on
+  // move on to next code
 }else{
-  die("User non-existant");
+  die("User does not exist");
 }
 
 // $conn->real_escape_string($_GET["username"]);
@@ -61,7 +85,11 @@ if(preg_match('/[a-zA-Z0-9]/', $_GET["username"])) {
 }
 $current = $conn->query("SELECT contacts FROM accounts WHERE id = '".$conn->real_escape_string($qid)."'");
 $current = $current->fetch_array(MYSQLI_NUM);
-$conn->query("UPDATE accounts SET contacts = '".$current[0]."&&&&&".$conn->real_escape_string($surl)."|||||".generateRandomString()."' WHERE id = '".$conn->real_escape_string($qid)."'");
+$vale = generateRandomString();
+$conn->query("UPDATE accounts SET contacts = '".$conn->real_escape_string($current[0])."&&&&&".$conn->real_escape_string($surl)."|||||".$vale."' WHERE id = '".$conn->real_escape_string($qid)."'");
+// Most servers add a .ht* by default to the apache file blacklist, this makes sure nobody can rotate scan through conversations (blocking access for plaintext), i bet its probably pretty easy to do
+file_put_contents("convos/.ht".$vale, "Conversation has been created! Nobody else can see these messages, only you and the other person.\n");
+$conn->query("UPDATE accounts SET contacts = '".$conn->real_escape_string($current[0])."&&&&&".$conn->real_escape_string($qusername)."|||||".$vale."' WHERE firstname = '".$conn->real_escape_string($_GET["addcontact"])."'");
 // First we add the url, then the username
 $conn->close();
 die("Success");
---
+++ convos/index.php
@@ -0,0 +1,1 @@
+you do not have access to this folder, by default conversations should be restricted from access without the api
