5a354a62bab41b9fd30aafac00fd5b0d53a33fc9
SadError256 <hylandb256@gmail.com>
2018-10-07 15:37:52 -0400

    Escape Newlines

--- home.php
+++ home.php
@@ -76,7 +76,7 @@ exit();
     setInterval(function() {
       if (tmpid != "EMPTY") {
         httpGet("api.php?username="+username+"&password="+password+"&readmessages="+tmpid, function(resu) {
-          var les = resu.replace("\n", "<br>");
+          var les = resu.replace(/\n/g, "<br>");
           document.getElementById("messbox").innerHTML = les;
         });
       }
