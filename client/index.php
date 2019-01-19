74bd05dfc252e1bc215a60d383be1578b57edaf0
nekobit1 <no.nekobit1@gmail.com>
2019-01-18 22:14:00 -0500

    Code cleanup 1

--- client/index.php
+++ client/index.php
@@ -25,7 +25,7 @@ require '../mysqlinfo.php';
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
 }
 $doesexist = $conn->prepare("SELECT id, username, password, anonid, contacts FROM accounts WHERE username = ?");
 $doesexist->bind_param('s', $_SESSION["usernamedata"]);
@@ -47,303 +47,53 @@ if ($doesexistnumrows >= 0 ) {
 		}
 	}
 }
-
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
-<!-- Starchat Web Client v0.8.0 -->
-
+<!-- Starchat Web Client v0.8.1 -->
 <head>
-  <title>Starchat</title>
-  <link rel="stylesheet" type="text/css" href="../libs/bootstrap/css/bootstrap.min.css">
-  <link id="stylesheeta" rel="stylesheet" type="text/css" href="../themes/default.css">
-
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Starchat</title>
+	<link rel="stylesheet" type="text/css" href="../libs/bootstrap/css/bootstrap.min.css">
+	<link id="stylesheeta" rel="stylesheet" type="text/css" href="../themes/default.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="../libs/jquery.min.js"></script>
 </head>
 <body>
-  <div id="messbox">
-	  <div id="messboxsmall">
+	<div id="messbox">
+		<div id="messboxsmall">
 
-	  </div>
-  </div>
-  <input type="text" id="chatbox" onkeypress="checkkey(event)">
-  <input type="button" value="Send" onclick="sendmessage()" id="chatboxbutton">
-  <div id="contacts">
+		</div>
+	</div>
+	<input type="text" id="chatbox" onkeypress="checkkey(event)">
+	<input type="button" value="Send" onclick="sendmessage()" id="chatboxbutton">
+	<div id="contacts"> <div class="loading"></div> </div>
 
-  </div>
+	<div id="topbar">
+		<img src="../img/hammenu.png" class="logo" onclick="opensettings()"> <span class="logotext">Starchat</span>
+	</div>
 
-  <div id="topbar">
-  <img src="../img/hammenu.png" class="logo" onclick="opensettings()"> <span class="logotext">Starchat</span>
-  </div>
+	<div id="callform">
 
-  <div id="callform">
+	</div>
 
-  </div>
-
-  <div id="settings">
-	<a href="#" onclick="closesettings()">Close Options</a>
-	<h1>Options</h1>
-	<p><select id="theme" onchange="themeChange();">
-    <option value="default">Default</option>
-    <option value="night-blue">Night Blue</option>
-    <option value="hacker-green">Hacker Green</option>
-    <option value="sky-blue">Sky Blue</option>
-    <option value="material-pink">Material Pink</option>
-  </select></p>
-	<p><input type="button" value="Add Contact" onclick="addacontact()" class="btn btn-light"><!-- TODO change this dialog to own function instead of alert --></p>
-	<p><input type="button" value="Add Contact to active conversation" disabled="disabled" onclick="addbcontact()" class="btn btn-light"> (group chats) (Currently Broken)</p>
-  <p><a href="../logout.php" class="btn btn-danger">Logout</a></p>
-  </div>
-  <script type="text/javascript">
-  	
-	// MIT License
-
-	// Copyright (c) 2018-2019 NekoBit
-
-	// Permission is hereby granted, free of charge, to any person obtaining a copy
-	// of this software and associated documentation files (the "Software"), to deal
-	// in the Software without restriction, including without limitation the rights
-	// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	// copies of the Software, and to permit persons to whom the Software is
-	// furnished to do so, subject to the following conditions:
-
-	// The above copyright notice and this permission notice shall be included in all
-	// copies or substantial portions of the Software.
-
-	// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	// SOFTWARE.
-
-	var tmpid = "EMPTY";
-	var calling = "no";
-	var jitsi = '<?php echo htmlspecialchars($jitsi); ?>';
-	var mesapn = [];
-	var oldmesapn = [];
-	var unread = 0;
-	var oldmessage = 0;
-
-	function opensettings() {
-		document.getElementById("settings").style.visibility = "visible";
-	}
-
-	function setCookie(cname, cvalue, exdays) {
-		var d = new Date();
-		d.setTime(d.getTime() + (exdays*24*60*60*1000));
-		var expires = "expires="+ d.toUTCString();
-		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-	}
-
-	function getCookie(cname) {
-		var name = cname + "=";
-		var decodedCookie = decodeURIComponent(document.cookie);
-		var ca = decodedCookie.split(';');
-		for(var i = 0; i <ca.length; i++) {
-			var c = ca[i];
-			while (c.charAt(0) == ' ') {
-				c = c.substring(1);
-			}
-			if (c.indexOf(name) == 0) {
-				return c.substring(name.length, c.length);
-			}
-		}
-		return "";
-	}
-
-	function swapSheet(sheet) {
-		document.getElementById("stylesheeta").setAttribute("href", sheet);
-	}
-
-	if (getCookie("starchattheme") != "") {
-		swapSheet(getCookie("starchattheme"));
-	}
-
-	var token = getCookie("stoken");
-
-	function themeChange() {
-		setCookie("starchattheme", "../themes/"+document.getElementById("theme").value+".css", 365);
-		swapSheet(getCookie("starchattheme"));
-	}
-
-	function startcall() {
-		if (calling == "yes") {
-			endcall();
-		}else{
-			document.getElementById("callform").innerHTML = "<button id='closeb' onclick='endcall()'>Close (end call)</button><iframe src='https://meet.jit.si/"+tmpid+"' allow='microphone; camera'>";
-			document.getElementById("callform").style.visibility = "visible";
-			calling = "yes";
-		}
-	}
-
-	function endcall() {
-		document.getElementById("callform").innerHTML = "";
-		document.getElementById("callform").style.visibility = "hidden";
-		calling = "no";
-	}
-
-	function closesettings() {
-		document.getElementById("settings").style.visibility = "hidden";
-	}
-
-	function checkkey(event) {
-		if (event.key == "Enter") {
-			sendmessage();
-		}
-	}
-
-	function httpGet(theUrl, callback) {
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.onreadystatechange = function() {
-		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-			callback(xmlHttp.responseText);
-		}
-		xmlHttp.open("GET", theUrl, true); // true for asynchronous, we need async to keep everything working
-		xmlHttp.send(null);
-	}
-
-	function splitcontacts() {
-		httpGet("../api/v1/?token="+token+"&getcontacts=yes", function(str) {
-
-		var contacts = JSON.parse(str);
-		for (var x = 0; x <= contacts.length; x++) {
-			document.getElementById("contacts").innerHTML += "<div class='box' onclick='switchcontact(\""+contacts[x][1]+"\")'><img src='../img/user.png' class='pfp'><div class='info'>"+contacts[x][0]+"</div></div>";
-		}
-		});
-	}
-
-	splitcontacts(); // the function is defined, we run the code, just once for now
-
-	function switchcontact(vals) {
-		if (tmpid == "EMPTY") {
-			if (jitsi == 'true') {
-				document.getElementById("topbar").innerHTML += "<img src='../img/call.png' id='call' onclick='startcall()'>";
-			}
-		}
-		tmpid = vals;
-	}
-
-  function meshan(x, contacts) {
-		httpGet("../api/v1/?token="+token+"&readmessages="+contacts[x][1]+"&count=5", function(data) {
-      mesapn[x] = data;
-      if (document.hasFocus() == false) {
-        if (oldmesapn[x] != mesapn[x]) {
-          var audio = new Audio('../sounds/notification.wav');
-          audio.play();
-          unread += 1;
-          document.title = "Starchat("+unread+"+)";
-        }
-      }
-      oldmesapn[x] = data;
-		});
-	}
-
-  if (mesapn == []) {
-    httpGet("../api/v1/?token="+token+"&getcontacts=yes", function(data) {
-      var contacts = JSON.parse(data);
-      for (var x = 0; x <= contacts.length-1; x++) {
-        meshan(x, contacts);
-      }
-    });
-    oldmesapn = [];
-  }
-
-
-
-
-	setInterval(function() {
-		if (tmpid != "EMPTY") {
-			httpGet("../api/v1/?token="+token+"&readmessages="+tmpid+"&count=25", function(resu) {
-				var les = JSON.parse(resu);
-				document.getElementById("messboxsmall").innerHTML = "";
-				var opres = 0;
-				for (var x = 0; x < les.length; x++) {
-					if (les[x].username == "<?php echo preg_replace('/[^a-zA-Z0-9]/', '', $_SESSION["usernamedata"]); ?>") {
-						opres = 0;
-						document.getElementById("messboxsmall").innerHTML += "<div style='clear:both;color:#ffffff;padding:5px;display:block;'><div class='smessage'>"+les[x].message+"</div></div>"; // In 0.8 we will include datetime float datetime to right
-					}else{
-						if (opres == 0) {
-							document.getElementById("messboxsmall").innerHTML += "<div style='clear:both;padding:5px;display:block;'><div class='susername'>"+les[x].username+"</div><div class='sopmessage'>"+les[x].message+"</div></div>"; // In 0.8 we will include datetime float datetime to right
-							opres = les[x].username;
-						}else{
-							if (opres == les[x].username) {
-							document.getElementById("messboxsmall").innerHTML += "<div style='clear:both;padding:5px;display:block;'><div class='sopmessage'>"+les[x].message+"</div></div>"; // In 0.8 we will include datetime float datetime to right
-							}else{
-							document.getElementById("messboxsmall").innerHTML += "<div style='clear:both;padding:5px;display:block;'><div class='susername'>"+les[x].username+"</div><div class='sopmessage'>"+les[x].message+"</div></div>"; // In 0.8 we will include datetime float datetime to right
-							opres = les[x].username;
-
-							}
-						}
-					}
-				}
-
-				// scroll to bottom
-        if (oldmessage != resu) {
-  				var objDiv = document.getElementById("messboxsmall");
-  				objDiv.scrollTop = objDiv.scrollHeight;
-        }
-        oldmessage = resu;
-      });
-		}
-
-    httpGet("../api/v1/?token="+token+"&getcontacts=yes", function(data) {
-      if (unread == 0) {
-        var contacts = JSON.parse(data);
-        for (var x = 0; x <= contacts.length-1; x++) {
-          meshan(x, contacts);
-        }
-      }
-    });
-
-		if (document.hasFocus()) {
-      unread = 0;
-      document.title = "Starchat";
-		}
-
-	},2000)
-
-	function sendmessage() {
-		var usermessage = document.getElementById("chatbox").value;
-		usermessage = encodeURIComponent(usermessage);
-		httpGet("../api/v1/?token="+token+"&sendmessage="+usermessage+"&sendmessageto="+tmpid, function() {
-			document.getElementById("chatbox").value = "";
-			unread = 0;
-			prevmsg = false;
-			httpGet("../api/v1/?token="+token+"&getcontacts=yes", function(data) {
-				var contacts = JSON.parse(data);
-				for (var x = 0; x <= contacts.length-1; x++) {
-				//console.log(contacts[x][1]);
-				meshan(x, contacts);
-				}
-			});
-		});
-	}
-
-	function addacontact() {
-		var toadd = encodeURIComponent(prompt("Please Enter Username to Add"));
-		httpGet("../api/v1/?token="+token+"&addcontact="+toadd, function(code) {
-			location.reload()
-		});
-	}
-
-	function addbcontact() {
-		if (tmpid == "EMPTY") {
-			alert("Please actually select a contact");
-		}else{
-			var toadd = prompt("Please Enter Username to Add");
-			httpGet("../api/v1/?token="+token+"&addtoconvo="+toadd+"&convoid="+tmpid, function(code) {
-				if (code != "\n0") {
-					alert("User not found or the conversation you are in does not exist (you should not get the second reason on the web client)");
-				}
-			});
-		}
-	}
-  </script>
+	<div id="settings">
+		<a href="#" onclick="closesettings()">Close Options</a>
+		<h1>Options</h1>
+		<p><select id="theme" onchange="themeChange();">
+			<option value="default">Default</option>
+			<option value="night-blue">Night Blue</option>
+			<option value="hacker-green">Hacker Green</option>
+			<option value="sky-blue">Sky Blue</option>
+			<option value="material-pink">Material Pink</option>
+		</select></p>
+		<p><input type="button" value="Add Contact" onclick="addacontact()" class="btn btn-light"><!-- TODO change this dialog to own function instead of alert --></p>
+		<p><input type="button" value="Add Contact to active conversation" disabled="disabled" onclick="addbcontact()" class="btn btn-light"> (group chats) (Currently Broken)</p>
+		<p><a href="../logout.php" class="btn btn-danger">Logout</a></p>
+	</div>
+	<script>
+		var jitsi = '<?php echo htmlspecialchars($jitsi); ?>';
+	</script>
+	<script src="client.js"></script>
 </body>
 </html>
--- setup.php
+++ setup.php
@@ -27,89 +27,89 @@ if(file_exists('mysqlinfo.php')) {
 
 if(isset($_POST["jitsi"])) {
 // $usetype = $_POST["usetype"];
-$usetype = "public";
+	$usetype = "public";
 
-$username = $_POST["username"];
-$password = $_POST["password"];
-$location = $_POST["location"];
-$dbname = $_POST["dbname"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$location = $_POST["location"];
+	$dbname = $_POST["dbname"];
 
 
-if ($usetype = "public") {
-$conn = new mysqli($location, $username, $password);
-if ($conn->connect_error) {
-die("Connection to MYSQL server failed! Reason: ".$conn->connect_error);
-}
+	if ($usetype = "public") {
+		$conn = new mysqli($location, $username, $password);
+		if ($conn->connect_error) {
+			die("Connection to MYSQL server failed! Reason: ".$conn->connect_error);
+		}
 
-if ($_POST["jitsi"] == "true") {
-$info = "
-<?php
+		if ($_POST["jitsi"] == "true") {
+			$info = "
+			<?php
 
-\$user = \"".addslashes($username)."\";
-\$pass = \"".addslashes($password)."\";
-\$mysqlurl = \"".addslashes($location)."\";
-\$dbname = \"".addslashes($dbname)."\";
-\$jitsi = \"true\";
-\$conn = new mysqli(\$mysqlurl, \$user, \$pass, \$dbname);
-?>
-";
-}else{
-$info = "
-<?php
+			\$user = \"".addslashes($username)."\";
+			\$pass = \"".addslashes($password)."\";
+			\$mysqlurl = \"".addslashes($location)."\";
+			\$dbname = \"".addslashes($dbname)."\";
+			\$jitsi = \"true\";
+			\$conn = new mysqli(\$mysqlurl, \$user, \$pass, \$dbname);
+			?>
+			";
+		}else{
+			$info = "
+			<?php
 
-\$user = \"".addslashes($username)."\";
-\$pass = \"".addslashes($password)."\";
-\$mysqlurl = \"".addslashes($location)."\";
-\$dbname = \"".addslashes($dbname)."\";
-\$jitsi = \"false\";
-\$conn = new mysqli(\$mysqlurl, \$user, \$pass, \$dbname);
-?>
-";
-}
+			\$user = \"".addslashes($username)."\";
+			\$pass = \"".addslashes($password)."\";
+			\$mysqlurl = \"".addslashes($location)."\";
+			\$dbname = \"".addslashes($dbname)."\";
+			\$jitsi = \"false\";
+			\$conn = new mysqli(\$mysqlurl, \$user, \$pass, \$dbname);
+			?>
+			";
+		}
 
-file_put_contents("mysqlinfo.php", $info);
+		file_put_contents("mysqlinfo.php", $info);
 
 // The database will not be created if it does not exist
 // Setup will crash otherwise
 // DO NOT use prepared statement to create object
 // We instead will use preg_replace and mysqli real escape string, that is safe enough
 // In the future, do not use real escape string, it is insecure
-$sql = "CREATE DATABASE IF NOT EXISTS ".$conn->real_escape_string($dbname);
-if ($conn->query($sql) === TRUE) {
+		$sql = "CREATE DATABASE IF NOT EXISTS ".$conn->real_escape_string($dbname);
+		if ($conn->query($sql) === TRUE) {
 	// Database created
-}else{
-	echo "Error creating database: " . $conn->error . "<br>";
-}
+		}else{
+			echo "Error creating database: " . $conn->error . "<br>";
+		}
 
-$conns = new mysqli($location, $username, $password, $dbname);
+		$conns = new mysqli($location, $username, $password, $dbname);
 
-$sql = "CREATE TABLE accounts (
-id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(70) NOT NULL,
-password VARCHAR(100) NOT NULL,
-anonid VARCHAR(70) NOT NULL,
-contacts VARCHAR(2100) NOT NULL
-)";
+		$sql = "CREATE TABLE accounts (
+		id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		username VARCHAR(70) NOT NULL,
+		password VARCHAR(100) NOT NULL,
+		anonid VARCHAR(70) NOT NULL,
+		contacts VARCHAR(2100) NOT NULL
+	)";
 
-if ($conns->query($sql) === TRUE) {
+	if ($conns->query($sql) === TRUE) {
 	// Tables created
-} else {
-    echo "Error creating table: " . $conns->error . "<br>";
-}
+	} else {
+		echo "Error creating table: " . $conns->error . "<br>";
+	}
 
 
-$sqla = "CREATE TABLE messages (
-id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-chatid VARCHAR(70) NOT NULL,
-dt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-username VARCHAR(50) NOT NULL,
-message VARCHAR(2100) NOT NULL
+	$sqla = "CREATE TABLE messages (
+	id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	chatid VARCHAR(70) NOT NULL,
+	dt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	username VARCHAR(50) NOT NULL,
+	message VARCHAR(2100) NOT NULL
 )";
 
 if ($conns->query($sqla) === TRUE) {
 	// Tables created
 } else {
-    echo "Error creating table: " . $conns->error . "<br>";
+	echo "Error creating table: " . $conns->error . "<br>";
 }
 
 $sqlb = "CREATE TABLE tokens (
@@ -122,7 +122,7 @@ created VARCHAR(50) NOT NULL
 if ($conns->query($sqlb) === TRUE) {
 	// Tables created
 } else {
-    echo "Error creating table: " . $conns->error . "<br>";
+	echo "Error creating table: " . $conns->error . "<br>";
 }
 
 $conns->close();
@@ -136,37 +136,37 @@ die("END");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Starchat Setup</title>
-<style>
-body {
-background-color: #afafaf;
-color: black;
-font-family: arial;
-}
-.pushed {
-padding: 60px;
-margin: 15px;
-background-color: #ffffff;
-border: 1px solid #000000;
-}
+	<title>Starchat Setup</title>
+	<style>
+	body {
+		background-color: #afafaf;
+		color: black;
+		font-family: arial;
+	}
+	.pushed {
+		padding: 60px;
+		margin: 15px;
+		background-color: #ffffff;
+		border: 1px solid #000000;
+	}
 </style>
 </head>
 <body>
-<div class="pushed">
-<h1>Starchat Install</h1>
-<p>You are here because you are doing the install. To proceed, the installer will create and setup mysql databases. Once this setup is done, starchat should be ready for production.</p>
-<i>Note: The setup file will be removed once you press submit</i>
-<form action="setup.php" method="post">
-<p>Lets setup some preferences first</p>
-<b>Do you want Jit.si support in web client (video calling)?</b><br>
-<input type="radio" name="jitsi" value="true" checked> Yes<br>
-<input type="radio" name="jitsi" value="false"> No<br><br>
-Your <b>MYSQL</b> user: <input type="text" name="username" value="root"><br>
-Your <b>MYSQL</b> password: <input type="password" name="password"><br>
-MYSQL server url: <input type="text" name="location" value="localhost"> (leave default if unsure or if you are currently on the same server where mysql is installed and running)<br>
-MYSQL Database name: <input type="text" name="dbname" value="starchat"><br><br>
-<input type="submit" value="Submit">
-</form>
-</div>
+	<div class="pushed">
+		<h1>Starchat Install</h1>
+		<p>You are here because you are doing the install. To proceed, the installer will create and setup mysql databases. Once this setup is done, starchat should be ready for production.</p>
+		<i>Note: The setup file will be removed once you press submit</i>
+		<form action="setup.php" method="post">
+			<p>Lets setup some preferences first</p>
+			<b>Do you want Jit.si support in web client (video calling)?</b><br>
+			<input type="radio" name="jitsi" value="true" checked> Yes<br>
+			<input type="radio" name="jitsi" value="false"> No<br><br>
+			Your <b>MYSQL</b> user: <input type="text" name="username" value="root"><br>
+			Your <b>MYSQL</b> password: <input type="password" name="password"><br>
+			MYSQL server url: <input type="text" name="location" value="localhost"> (leave default if unsure or if you are currently on the same server where mysql is installed and running)<br>
+			MYSQL Database name: <input type="text" name="dbname" value="starchat"><br><br>
+			<input type="submit" value="Submit">
+		</form>
+	</div>
 </body>
 </html>
