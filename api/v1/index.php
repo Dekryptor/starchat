b4eea14fd1ea3cdbe667e5ead84077ed47655ae9
nekobit1 <no.nekobit1@gmail.com>
2019-03-03 14:47:35 -0500

    Revert "Mysqli chaining"
    This reverts commit bee2065e03157363f41b14bb4698dbe75dd30a31.

--- api/v1/index.php
+++ api/v1/index.php
@@ -51,8 +51,9 @@ function generateRandomString($length = 32) {
 
 // API Login System
 if (isset($_GET["username"])) {
-	$check_login = $conn->prepare("SELECT id, username, password, anonid, contacts FROM accounts WHERE username = ?")
-		->bind_param('s', $_GET["username"])->execute();
+	$check_login = $conn->prepare("SELECT id, username, password, anonid, contacts FROM accounts WHERE username = ?");
+	$check_login->bind_param('s', $_GET["username"]);
+	$check_login->execute();
 	$check_login_results = $check_login->get_result();
 	$check_login_numrows = $check_login_results->num_rows;
 
@@ -66,8 +67,9 @@ if (isset($_GET["username"])) {
 					$token = generateRandomString();
 					$token_date = date("Y-m-d H:i:s");
 
-					$token_check = $conn->prepare("SELECT token FROM tokens WHERE token=?")
-						->bind_param('s', $token)->execute();
+					$token_check = $conn->prepare("SELECT token FROM tokens WHERE token=?");
+					$token_check->bind_param('s', $token);
+					$token_check->execute();
 					$token_check_results = $token_check->get_result();
 					$token_check_rows = $token_check_results->num_rows;
 
@@ -75,9 +77,9 @@ if (isset($_GET["username"])) {
 						starchat_error("Token has been used, please try again.");
 					}
 
-					$token_query = $conn->prepare("INSERT INTO tokens (token, username, created) VALUES (?, ?, ?)")
-						->bind_param('sss', $token, $_GET["username"], $token_date)->execute();
-					// Display token
+					$token_query = $conn->prepare("INSERT INTO tokens (token, username, created) VALUES (?, ?, ?)");
+					$token_query->bind_param('sss', $token, $_GET["username"], $token_date);
+					$token_query->execute();
 					echo $token;
 					$conn->close();
 					exit();
@@ -88,16 +90,18 @@ if (isset($_GET["username"])) {
 }
 
 if (isset($_GET["token"])) {
-	$token_use = $conn->prepare("SELECT * FROM tokens WHERE token=?")
-		->bind_param('s', $_GET["token"])->execute();
+	$token_use = $conn->prepare("SELECT * FROM tokens WHERE token=?");
+	$token_use->bind_param('s', $_GET["token"]);
+	$token_use->execute();
 	$token_use_results = $token_use->get_result();
 	$token_use_rows = $token_use_results->num_rows;
 
 	if ($token_use_rows == 1) {
 		while($row = $token_use_results->fetch_assoc()) {
 			if (strtotime($row["created"]) > strtotime("-24 hours")) {
-				$retrieve_info = $conn->prepare("SELECT * FROM accounts WHERE username=?")
-					->bind_param('s', $row['username'])->execute();
+				$retrieve_info = $conn->prepare("SELECT * FROM accounts WHERE username=?");
+				$retrieve_info->bind_param('s', $row['username']);
+				$retrieve_info->execute();
 				$retrieve_info_results = $retrieve_info->get_result();
 				$retrieve_info_rows = $retrieve_info_results->num_rows;
 
@@ -126,7 +130,6 @@ if (isset($_GET["token"])) {
 
 // API Features
 
-// Set to json
 header('Content-type: application/json');
 
 // Make sure username and password is set
@@ -161,8 +164,10 @@ if (isset($_GET["readmessages"])) {
 	}
 	$readmessage = $_GET["readmessages"];
 
-	$keep = $conn->prepare("SELECT * FROM messages WHERE chatid = ?")
-		->bind_param('s', $readmessage)->execute()->get_result();
+	$checkx = $conn->prepare("SELECT * FROM messages WHERE chatid = ?");
+	$checkx->bind_param('s', $readmessage);
+	$checkx->execute();
+	$keep = $checkx->get_result();
 
 	$start_count = $keep->num_rows - $read_count;
 
@@ -193,8 +198,10 @@ if (isset($_GET["sendmessage"])) {
 		starchat_error("The sender conversation id is invalid");
 	}
 
-	$checka = $conn->prepare("SELECT contacts FROM accounts WHERE username = ?")
-		->bind_param('s', $qusername)->execute()->get_result();
+	$checka = $conn->prepare("SELECT contacts FROM accounts WHERE username = ?");
+	$checka->bind_param('s', $qusername);
+	$checka->execute();
+	$cheeka = $checka->get_result();
 
 	$confirm = 0;
 	$vale = preg_replace("/^[a-zA-Z0-9\-]/","",$_GET["sendmessageto"]);
@@ -216,29 +223,36 @@ if (isset($_GET["sendmessage"])) {
 	$messagef = htmlspecialchars($_GET["sendmessage"]);
 	$susername = htmlspecialchars($qusername);
 
-	$getn = $conn->prepare("INSERT INTO messages (chatid, username, message) VALUES (?, ?, ?)")
-		->bind_param('sss', $chatid, $susername, $messagef)->execute();
+	$getn = $conn->prepare("INSERT INTO messages (chatid, username, message) VALUES (?, ?, ?)");
+	$getn->bind_param('sss', $chatid, $susername, $messagef);
+	$getn->execute();
 
+	echo "0";
 	$conn->close();
 	exit();
 }
 
 if (isset($_GET["addcontact"])) {
+
 	$surl = $_GET["addcontact"];
 
-	$quickcheck = $conn->prepare("SELECT username, id FROM accounts WHERE username = ?")
-		->bind_param('s', $_GET["addcontact"])->execute()->get_result();
+	$checkf = $conn->prepare("SELECT username, id FROM accounts WHERE username = ?");
+	$checkf->bind_param('s', $_GET["addcontact"]);
+	$checkf->execute();
+	$quickcheck = $checkf->get_result();
 
-	if (!($quickcheck->num_rows > 0)) {
-		starchat_error("Contact not found");
-		$conn->close();
+	if ($quickcheck->num_rows>0) {
+		// move on to next code
+	}else{
+		echo "1";
 		exit();
 	}
 
 	if(!preg_match('/[a-zA-Z0-9]/', $_GET["addcontact"])) {
-		starchat_error("The username is not valid and contains invalid characters");
 		$conn->close();
+		echo "2";
 		exit();
+		starchat_error("The username is not valid and contains invalid characters");
 	}
 	$vale = generateRandomString();
 	$messagesa = $conn->query("SELECT * FROM accounts"); // For something as simple as this, we should be fine to use query
@@ -249,12 +263,16 @@ if (isset($_GET["addcontact"])) {
 			}
 		}
 	}
+	$checka = $conn->prepare("SELECT contacts FROM accounts WHERE id = ?");
+	$checka->bind_param('s', $qid);
+	$checka->execute();
+	$current = $checka->get_result();
 
-	$current = $conn->prepare("SELECT contacts FROM accounts WHERE id = ?")
-		->bind_param('s', $qid)->execute()->$checka->get_result();
 
-	$current2 = $conn->prepare("SELECT contacts FROM accounts WHERE username = ?")
-		->bind_param('s', $_GET["addcontact"])->execute()->get_result();
+	$checkc = $conn->prepare("SELECT contacts FROM accounts WHERE username = ?");
+	$checkc->bind_param('s', $_GET["addcontact"]);
+	$checkc->execute();
+	$current2 = $checkc->get_result();
 
 	$saddcontact = htmlspecialchars($_GET["addcontact"]);
 
@@ -272,14 +290,17 @@ if (isset($_GET["addcontact"])) {
 		$currentb[$amo][1] = $vale;
 	}
 
-	$current = json_encode($currenta, JSON_PRETTY_PRINT);
-	$current2 = json_encode($currentb, JSON_PRETTY_PRINT);
-	
-	$cu1 = $conn->prepare("UPDATE accounts SET contacts = ? WHERE id = ?")
-		->bind_param('ss', $current, $qid)->execute();
+	$currenta = json_encode($currenta, JSON_PRETTY_PRINT);
+	$currentb = json_encode($currentb, JSON_PRETTY_PRINT);
+	$cu1 = $conn->prepare("UPDATE accounts SET contacts = ? WHERE id = ?");
+	$cu1->bind_param('ss', $currenta, $qid);
+	$cu1->execute();
 
-	$cu2 = $conn->prepare("UPDATE accounts SET contacts = ? WHERE username = ?")
-		->bind_param('ss', $current2, $saddcontact)->execute();
+
+	$cu2 = $conn->prepare("UPDATE accounts SET contacts = ? WHERE username = ?");
+	$cu2->bind_param('ss', $currentb, $saddcontact);
+
+	$cu2->execute();
 
 	$conn->close();
 	exit();
